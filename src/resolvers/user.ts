56d2c43bc47a000d837ee7b57b94fa01e2c893bc
Nolan Cong <ncongnolancong@gmail.com>
2020-11-24 13:18:02 -0800

    login and register mutations

---
+++ src/resolvers/user.ts
@@ -0,0 +1,78 @@
+import { MyContext } from "src/types";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
+import { User } from "./entities/User";
+import argon2 from 'argon2';
+@InputType()
+class UsernamePasswordInput {
+    @Field()
+    username: string;
+    @Field()
+    password: string;
+}
+
+//error in a specific field
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+    @Field()
+    message: string;
+}
+
+//user returned if it works propery, or an error to be return if it faisl
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[]
+
+    @Field(() => User, { nullable: true })
+    user?: User
+}
+
+@Resolver()
+export class UserResolver {
+    @Mutation(() => User)
+    async register(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() { em }: MyContext
+    ) {
+        const hashedPassword = await argon2.hash(options.password)
+        const user = em.create(User,
+            {
+                username: options.username,
+                password: hashedPassword
+            });
+        await em.persistAndFlush(user);
+        return user;
+    }
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() { em }: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, { username: options.username });
+        if (!user) {
+            return {
+                errors: [{
+                    field: "username",
+                    message: "Username does not exist",
+                }],
+            };
+        }
+        const valid = await argon2.verify(user.password, options.password);
+        if (!valid) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "Invalid Login",
+                    },
+                ],
+            };
+        }
+        return {
+            user
+        };
+    }
+}
